892c760be03639d5df2a49548ec76a70fa5e4dd4
chen <92xiaodao@gmail.com>
2024-06-28 17:37:44 +0800

    feat: ✨ update env controller

--- src/main/java/com/example/services/controller/EnvController.java
+++ src/main/java/com/example/services/controller/EnvController.java
@@ -1,18 +1,55 @@
 package com.example.services.controller;
 
+import com.example.services.entity.Env;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("env")
 public class EnvController {
 
-    // 新增
+    @PostMapping("")
+    public Resp<Void> createEnv(@RequestBody Env env) {
+        return Resp.success(null);
+    }
 
-    // 删除
+    @DeleteMapping("/{id}")
+    public Resp<Void> deleteEnv(@PathVariable Long id) {
+        return Resp.success(null);
+    }
 
-    // 更新
+    @PutMapping("")
+    public Resp<Void> updateEnvById(@RequestBody Env env) {
+        return Resp.success(null);
+    }
 
-    // 批量新增
+    @GetMapping("/{id}")
+    public Resp<Void> getEnvById(@PathVariable String id) {
+        return Resp.success(null);
+    }
 
-    // 批量删除
+    @PostMapping("/list")
+    public Resp<Void> batchAddEnv(@RequestBody List<Env> envList) {
+        return Resp.success(null);
+    }
 
-    // 批量更新
+    @DeleteMapping("/list")
+    public Resp<Void> batchDeleteEnv(@PathVariable List<String> ids) {
+        return Resp.success(null);
+    }
 
-    // 分页查找
+    @PutMapping("/list")
+    public Resp<Void> batchUpdateEnv(@RequestBody List<Env> envList) {
+        return Resp.success(null);
+    }
+
+    @GetMapping("/list")
+    public Resp<Void> getEnvList(
+            @RequestParam(value = "currentPage", defaultValue = "1") int currentPage,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+            @RequestParam(required = false) String queryCondition
+    ) {
+        return Resp.success(null);
+    }
 }
