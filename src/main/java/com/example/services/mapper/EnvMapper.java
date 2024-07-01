7d015274c15ebe2378adee40ebac742d05bf1a95
chen <92xiaodao@gmail.com>
2024-07-01 18:08:04 +0800

    feat: ✨ update

--- src/main/java/com/example/services/mapper/EnvMapper.java
+++ src/main/java/com/example/services/mapper/EnvMapper.java
@@ -12,16 +12,12 @@ import java.util.List;
 @Mapper
 public interface EnvMapper {
 
-    @Insert("insert into envs(name) values(#{name})")
     public Void addEnv(@Param("env") Env env);
 
-    @Delete("delete from envs where id = #{id}")
     public Void deleteEnvById(@Param("id") Long id);
 
-    @Update("update env set id = #{id}")
     public Void updateEnvById(@Param("env") Env env);
 
-    @Select("select * from envs where id = #{id}")
     public Env getEnvById(@Param("id") Long id);
 
     public Void batchAddEnvs(@Param("env") List<Env> envList);
